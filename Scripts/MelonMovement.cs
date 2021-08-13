9caa42c387ee9cc9b3a637e44861bca4951435eb
ArdaGurcan <ardagurcan2004@gmail.com>
2021-08-13 11:36:49 +0300

    add files

---
+++ Scripts/MelonMovement.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class MelonMovement : MonoBehaviour
+{
+    int phase = -1;
+    Vector3 target;
+    bool move;
+    float speed = 0.05f;
+    GameObject player;
+    public Text speechBubble;
+    public GameObject bubble;
+    public bool hitSnake;
+
+    string[] speech = {
+        "...",
+        "Dude...",
+        "STOP!",
+        "WHY ARE YOU TRYING TO EAT ME!?",
+        "Enough!",
+        "If you want to eat me, you'll have to defeat me first!"
+    };
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+
+        if (Mathf.Abs(player.transform.rotation.eulerAngles.z) == 90)
+        {
+            target = new Vector3(transform.position.x, Random.Range(-12, 12) * 0.3f);
+        }
+        else if (Mathf.Abs(player.transform.rotation.eulerAngles.z) != 90)
+        {
+            target = new Vector3(Random.Range(-16, 16) * 0.3f, transform.position.y);
+        }
+    }
+
+    void Update()
+    {
+        
+        if(move)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target, speed);
+            if(Vector3.Distance(transform.position,target) < speed || hitSnake)
+            {
+                if(hitSnake)
+                {
+                    Debug.Log("Hit by melon.");
+                }
+                transform.position = new Vector3(transform.position.x - transform.position.x % 0.3f, transform.position.y - transform.position.y % 0.3f, 0);
+                hitSnake = false;
+                move = false;
+
+            }
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+	{
+        if(other.tag == "Player")
+        {
+            
+
+            player.GetComponent<SnakeMovement>().stop = false;
+            if(!move)
+            {
+                
+
+                if (Mathf.Abs(player.transform.rotation.eulerAngles.z - 90) < 10 || Mathf.Abs(player.transform.rotation.eulerAngles.z - 270) < 10)
+                {
+                    target = new Vector3(Random.Range(-16, 16) * 0.3f, transform.position.y);
+                    Debug.Log(player.transform.rotation.eulerAngles.z);
+                }
+                else if (Mathf.Abs(player.transform.rotation.eulerAngles.z - 180) < 10 || Mathf.Abs(player.transform.rotation.eulerAngles.z) < 10)
+                {
+                    Debug.Log(player.transform.rotation.eulerAngles.z);
+                    target = new Vector3(transform.position.x, Random.Range(-12, 12) * 0.3f);
+                }
+
+                Debug.Log(target);
+            }
+            move = true;
+
+
+
+
+        }
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        bubble.SetActive(true);
+        phase++;
+        speechBubble.text = speech[phase];
+    }
+
+}
---
+++ Scripts/SnakeMovement.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnakeMovement : MonoBehaviour
+{
+    [SerializeField]Vector3 moveDirection;
+    public Vector3 currentDirection;
+    [SerializeField]float timePassed;
+    float maxTime = 0.2f;
+    public GameObject bodyPrefab;
+    [SerializeField]List<GameObject> bodyTiles = new List<GameObject>();
+    public bool stop;
+
+    void Start()
+    {
+        for (int i = 0; i < 9; i++)
+        {
+            GameObject tile = Instantiate(bodyPrefab, transform.position - new Vector3((i + 1) * transform.localScale.x, 0), Quaternion.identity);
+            tile.transform.localScale = transform.localScale;
+            bodyTiles.Add(tile);
+        }
+    }
+
+    void Update()
+    {
+        //if(transform.position.x < 95.2 || transform.position.x > 104.8 ||
+        //   transform.position.y < -3.3 || transform.position.y > 3.3)
+        //{
+        //    stop = true;
+        //    GameOver();
+        //}
+            
+        if(!stop)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow) && currentDirection.y == 0)
+            {
+                moveDirection = new Vector3(0, 1);
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow) && currentDirection.y == 0)
+            {
+                moveDirection = new Vector3(0, -1);
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow) && currentDirection.x == 0)
+            {
+                moveDirection = new Vector3(1, 0);
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow) && currentDirection.x == 0)
+            {
+                moveDirection = new Vector3(-1, 0);
+            }
+
+            timePassed += Time.deltaTime;
+
+            if (timePassed >= maxTime && moveDirection != Vector3.zero)
+            {
+                currentDirection = moveDirection;
+                for (int i = 0; i < bodyTiles.Count; i++)
+                {
+                    if (bodyTiles[i].transform.position == transform.position + currentDirection * transform.localScale.x)
+                    {
+                        GameOver();
+                        break;
+                    }
+                }
+                if(transform.position.x + currentDirection.x * transform.localScale.x < -5.1 || transform.position.x + currentDirection.x * transform.localScale.x > 5.1 ||
+                   transform.position.y + currentDirection.y * transform.localScale.x < -3.6 || transform.position.y + currentDirection.y * transform.localScale.x > 3.6)
+                {
+                    GameOver();
+                }
+                if (!stop)
+                {
+                    
+                    timePassed = 0;
+                    GameObject tile = Instantiate(bodyPrefab, transform.position, Quaternion.identity);
+                    tile.transform.localScale = transform.localScale;
+                    bodyTiles.Insert(0, tile);
+                    Destroy(bodyTiles[bodyTiles.Count - 1]);
+                    bodyTiles.RemoveAt(bodyTiles.Count - 1);    
+                    transform.position += currentDirection * transform.localScale.x;
+                    if(currentDirection.y == 1)
+                    {
+                        transform.rotation = Quaternion.Euler(0, 0, 90);
+                    }
+                    else if (currentDirection.y == -1)
+                    {
+                        transform.rotation = Quaternion.Euler(0, 0, -90);
+                    }
+                    else if (currentDirection.x == 1)
+                    {
+                        transform.rotation = Quaternion.Euler(0, 0, 0);
+                    }
+                    else if (currentDirection.x == -1)
+                    {
+                        transform.rotation = Quaternion.Euler(0, 0, -180);
+                    }
+
+                }
+
+            }
+        }
+
+        void GameOver()
+        {
+            Debug.Log("Game Over :(");
+            stop = true;
+        }
+    }
+}
